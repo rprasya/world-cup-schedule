3442d5dbbd1c206aa643b34d5b1c741b0ec1aab7
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-09-26 16:00:17 +0700

    feat: add versus and match time to card component

--- components/card.tsx
+++ components/card.tsx
@@ -1,11 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
 
+import moment from "moment";
+
 interface cardProps {
-  match: Match;
+  match?: Match;
+  isToday?: boolean;
 }
 
 interface teamProps {
-  team: Team;
+  team?: Team;
+}
+
+interface VersusProps {
+  date?: string;
 }
 
 const Team = ({ team }: teamProps) => {
@@ -16,8 +23,19 @@ const Team = ({ team }: teamProps) => {
         alt={team?.name}
         className="w-[90px] h-[60px] border-[4px] border-black shadow-[5px_5px_0_0_#000000] rounded-[14px]"
       />
-      <p className="font-bold text-lg mt-2 text-center">{team.name}</p>
-      <p className="font-bold text-2xl text-center">{team.goals}</p>
+      <p className="font-bold text-lg mt-2 text-center">{team?.name}</p>
+      <p className="font-bold text-2xl text-center">{team?.goals}</p>
+    </div>
+  );
+};
+
+const Versus = ({ date }: VersusProps) => {
+  return (
+    <div className="grid relative h-full py-3">
+      <span className="absolute font-bold text-2xl place-self-center">VS</span>
+      <span className="font-medium text-2xl place-self-end bg-black text-white rounded-full px-3 py-1">
+        {moment(date).format("HH:mm")}
+      </span>
     </div>
   );
 };
@@ -25,8 +43,9 @@ const Team = ({ team }: teamProps) => {
 const Card = ({ match }: cardProps) => {
   return (
     <div className="flex aspect-video border-black border-[3px] shadow-[8px_8px_0_0_#000000] hover:shadow-[15px_15px_0_0_#000000] rounded-[12px] transition-shadow">
-      <Team team={match.home_team} />
-      <Team team={match.away_team} />
+      <Team team={match?.home_team} />
+      <Versus date={match?.datetime} />
+      <Team team={match?.away_team} />
     </div>
   );
 };
